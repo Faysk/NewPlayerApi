059df4243f1be611ad424607c45cfb05fc1d9316
RenanSilva <faysk.nan@gmail.com>
2020-05-02 14:46:55 -0300

    Fix

---
+++ FayskFlixBack/Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FayskFlixBack.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public FilesController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string[] Get()
+        {
+            var arquivos = Directory.GetFiles("C:\\blob\\midia");
+            List<string> retorno = new List<string>();
+
+            foreach(var arquivo in arquivos)
+            {
+                retorno.Add(Path.GetFileName(arquivo));
+            }
+
+            return retorno.ToArray();
+        }
+    }
+}
